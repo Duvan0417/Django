7c3b58f942868c49928ea500031102d1efde70d8
Duvan0417 <narvaezduvan29@gmail.com>
2025-11-04 11:44:18 -0500

    configuracion de vistas

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello Word</h1>")
+def index(request):
+    contexto = {"titulo":"Pagina de Inicio"}
+    return render(request, "index/index.html", contexto)
+def hello(request, id):
+    print(type(id))
+    return HttpResponse("<h1>Hello %s</h1>" % id)
 
 def about(request):
     return HttpResponse("<h1>about</h1>")
